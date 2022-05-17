5a5136f679d1ec4b3ec5a9726aeec392d8d4ab27
gness1804 <graham.nessler@gmail.com>
2022-05-17 11:43:17 -0500

    all tests passing

--- flatten-array.ts
+++ flatten-array.ts
@@ -1,3 +1,13 @@
-export function flatten(arr: number[]) {
+type RecursiveArray = Array<RecursiveArray | number | undefined>;
 
+export function flatten(arr: RecursiveArray, priorRes?: number[]): number[] {
+  const res: number[] = priorRes || [];
+  arr.forEach(item => {
+    if (Array.isArray(item)) {
+      flatten(item, res);
+    } else if (item !== undefined) {
+      res.push(item);
+    }
+  })
+  return res;
 }
